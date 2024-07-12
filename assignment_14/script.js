3631b92da968319baf8fbd3d790b9ebdbdcde773
Maaz Ai-enthusiast <maaz.ai.enthusiast@gmail.com>
2024-07-12 13:49:20 -0700

    array ques

---
+++ assignment_14/script.js
@@ -0,0 +1,79 @@
+
+
+
+// let arr1=[1,2,3,4,5,6,7,8,9];
+// let arr2=arr1;
+// console.log(arr2);
+
+
+// let negative =[-1,1,-2,2,-3,3,-4,4,-5,5];
+// let n=0;
+// negative.forEach(element => {
+//     if(element<0){
+// n++;
+//     }
+// });
+// console.log(`Total negative number of elements is ${n}`);
+
+
+
+
+
+// let even =0;
+// let odd=0;
+// let arr=[1,2,3,4,5,6,7,8,9];
+
+// arr.forEach(element => {
+
+// if(element%2==0){
+//     even++;
+// }
+// else {
+//     odd++;
+// }
+
+// })
+// console.log(`Total even is ${even} and odd is ${odd}`);
+
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+// arr.sort((a,b)=>{
+//    a-b;
+// })
+// console.log(`Maximum number is ${arr[arr.length-1]}`);
+// console.log(` Second Maximum number is ${arr[arr.length-2]}`);
+
+
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+// arr.sort((a,b)=>{
+//    a-b;
+// })
+// console.log(`Maximum number is ${arr[arr.length-1]}`);
+// console.log( `Minimum number is ${arr[0]}` );  
+
+
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+// let sum=0;
+// arr.forEach(element => {
+//     sum += element;
+// });
+
+// console.log( ` sum: ${sum}` );
+
+
+
+
+// let negative =[-1,1,-2,2,-3,3,-4,4,-5,5];
+// negative.forEach(element => {
+//     if(element<0){
+// console.log(element);
+//     }
+// });
+
+
+// let arr=[1,2,3,4,5,6,7,8,9];
+// for (const n of arr) {
+//     console.log(n);
+// }
